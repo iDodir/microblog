705d5f930147d1631f25553654fd2f8c31d06eb1
dodir <mokronosovmw@gmail.com>
2022-10-05 17:39:41 +0500

    add email support and bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,11 @@ import os
 from logging.handlers import SMTPHandler, RotatingFileHandler
 
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
+from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
@@ -17,6 +19,8 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 from app import routes, models, errors
 
